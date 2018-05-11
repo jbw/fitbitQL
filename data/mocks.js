6590c173e46b5b488cc34ed7be184629a95293d8
Jason Watson <jbw@users.noreply.github.com>
2018-05-11 10:47:01 +0100

    add activity summary types

--- data/mocks.js
+++ data/mocks.js
@@ -4,6 +4,24 @@ const mocks = {
   Step: () => ({
     count: 1000,
   }),
+
+  Summary: () => ({
+    steps: 1000,
+    elevation: 12.34,
+    fairlyActiveMinutes: 100,
+    floors: 12,
+    lightlyActiveMinutes: 99,
+    marginalCalories: 12,
+    sedentaryMinutes: 24,
+    veryActiveMinutes: 48,
+    distances: [
+      {
+        activity: 'total',
+        distance: 7.96,
+      },
+    ],
+
+  }),
 };
 
 export default mocks;
